ece9e5959a88321ae85b9826f91037794d9999ed
jscodeguy <im.still.at.school99@gmail.com>
2022-04-24 18:36:46 -0400

    update item route working

--- app/routes/item_routes.js
+++ app/routes/item_routes.js
@@ -72,5 +72,29 @@ router.post('/items', requireToken, (req, res, next) => {
 		.catch(next)
 })
 
+// UPDATE
+// PATCH /items/5a7db6c74d55bc51bdf39793
+router.patch('/items/:id', requireToken, removeBlanks, (req, res, next) => {
+	// if the client attempts to change the `owner` property by including a new
+	// owner, prevent that by deleting that key/value pair
+	delete req.body.item.owner
+
+	Item.findById(req.params.id)
+		.then(handle404)
+		.then((item) => {
+			// pass the `req` object and the Mongoose record to `requireOwnership`
+			// it will throw an error if the current user isn't the owner
+			requireOwnership(req, item)
+
+			// pass the result of Mongoose's `.update` to the next `.then`
+			return item.updateOne(req.body.item)
+		})
+		// if that succeeded, return 204 and no JSON
+		.then(() => res.sendStatus(204))
+		// if an error occurs, pass it to the handler
+		.catch(next)
+})
+
+
 
 module.exports = router
